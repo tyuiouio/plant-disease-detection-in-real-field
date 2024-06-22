25dc0aaf2cb6106eb8226318c8424c94e517011b
tyuiouio <47018333+tyuiouio@users.noreply.github.com>
2024-06-22 23:07:46 +0800

    Add files via upload

---
+++ anchor.py
@@ -0,0 +1,7 @@
+import torch
+from models.experimental import attempt_load
+
+model = attempt_load('./best.pt', map_location=torch.device('cpu'))
+m = model.module.model[-1] if hasattr(model, 'module') else model.model[-1]
+print(m.anchor_grid)
+
---
+++ example.py
@@ -0,0 +1,8 @@
+import detect
+import os
+import shutil
+
+
+result = detect.det_yolov5v6('./best.pt','./images',"./Temporary_folder")
+#print(result)
+
